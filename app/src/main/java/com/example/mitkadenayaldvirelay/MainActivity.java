a01973c74a68986139b8580819dc071d3402f557
elayben <elayben@gmail.com>
2024-05-29 17:28:10 +0300

    adding the list features

--- app/src/main/java/com/example/mitkadenayaldvirelay/MainActivity.java
+++ app/src/main/java/com/example/mitkadenayaldvirelay/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.mitkadenayaldvirelay;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mitkadenayaldvirelay.adapters.VideoListAdapter;
+import com.example.mitkadenayaldvirelay.classes.Video;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView videoList = findViewById(R.id.videoList);
+        final VideoListAdapter adapter = new VideoListAdapter(this);
+        videoList.setAdapter(adapter);
+        videoList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        List<Video> videos = new ArrayList<>();
+        videos.add(new Video("One", "Football", "Hapoel win", 4, R.drawable.login));
+        videos.add(new Video("Two", "Tennis", "Hapoel draw", 9, R.drawable.login));
+        videos.add(new Video("Three", "Basket", "Hapoel lose", 7, R.drawable.login));
+        adapter.setVideos(videos);
+
     }
 }
---
+++ app/src/main/java/com/example/mitkadenayaldvirelay/classes/Video.java
@@ -0,0 +1,95 @@
+package com.example.mitkadenayaldvirelay.classes;
+
+import android.media.Image;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import com.example.mitkadenayaldvirelay.R;
+
+public class Video {
+
+    private static int idCounter = 0;
+    private final int id;
+    private String channel;
+    private int likes;
+    private int views;
+    private String title;
+    private String description;
+    private int duration;
+    private int thumbnail;
+
+    public Video() {
+        this.id = ++idCounter;
+        this.thumbnail = R.drawable.login;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+
+
+    public Video(String channel, String title, String description, int duration, int thumbnail) {
+        this.id = ++idCounter;
+        this.channel = channel;
+        this.likes = 0;
+        this.views = 0;
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+        this.thumbnail = thumbnail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+}
