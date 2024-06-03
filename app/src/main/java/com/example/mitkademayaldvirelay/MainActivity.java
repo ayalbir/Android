ed294f2821165ec11f645883dd298a295fae92bb
elayben <elayben@gmail.com>
2024-06-04 02:38:24 +0300

    add side menu

--- app/src/main/java/com/example/mitkademayaldvirelay/MainActivity.java
+++ app/src/main/java/com/example/mitkademayaldvirelay/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.mitkademayaldvirelay;
 
 import static com.example.mitkademayaldvirelay.R.layout.activity_main;
 
-import android.content.res.AssetManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mitkademayaldvirelay.adapters.VideoListAdapter;
 import com.example.mitkademayaldvirelay.classes.Video;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,7 +31,9 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,24 @@ public class MainActivity extends AppCompatActivity {
         videoList.setLayoutManager(new LinearLayoutManager(this));
 
         List<Video> videos = new ArrayList<>();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        // Set the initial checked item in the navigation view
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
 
         try {
             JSONObject obj = new JSONObject(loadJSONFromAsset());
@@ -54,7 +85,6 @@ public class MainActivity extends AppCompatActivity {
     public String loadJSONFromAsset() {
         String json = null;
         try {
-            //TODO: check how to write this line below
             InputStream is = getResources().getAssets().open("data.json");
             int size = is.available();
             byte[] buffer = new byte[size];
@@ -66,5 +96,41 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
         return json;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Toast.makeText(this, "Login selected", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_login) {
+            //TODO:add login activity
+            Toast.makeText(this, "Login selected", Toast.LENGTH_SHORT).show();
+            /*
+             Intent intent = new Intent(this, LoginActivity.class);
+             startActivity(intent);
+             */
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+      if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+          drawerLayout.closeDrawer(GravityCompat.START);
+      }
+      else
+          super.onBackPressed();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
